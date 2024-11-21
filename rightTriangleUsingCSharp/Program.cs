2857f058811164b20be8816ae6bf226d99bc75df
charlesdominique <charlesdominiquebacanto@gmail.com>
2024-11-21 15:48:58 +0800

    Add project files.

---
+++ rightTriangleUsingCSharp/Program.cs
@@ -0,0 +1,26 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Please enter a character: ");
+        char character = Convert.ToChar(Console.ReadLine());
+        Console.Write("Please enter the max amount: ");
+        int maxValue = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 0; i <= maxValue; i++)
+        {
+           Console.WriteLine(clone(character, i));
+        }
+    }
+    
+
+    static string clone(char c, int value)
+    {
+        string cloned ="";
+        for(int i = 0; i < value; i++)
+        {
+            cloned += c;
+        }
+        return cloned;
+    }
+}
