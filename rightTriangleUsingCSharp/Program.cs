da68633cd2ed7fa0118bccd5d076041a15151ada
charlesdominique <charlesdominiquebacanto@gmail.com>
2024-11-21 15:50:30 +0800

    Added Comments

--- rightTriangleUsingCSharp/Program.cs
+++ rightTriangleUsingCSharp/Program.cs
@@ -2,19 +2,19 @@
 {
     static void Main(string[] args)
     {
-        Console.Write("Please enter a character: ");
-        char character = Convert.ToChar(Console.ReadLine());
-        Console.Write("Please enter the max amount: ");
-        int maxValue = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Please enter a character: "); // Prompt the User to enter a character
+        char character = Convert.ToChar(Console.ReadLine()); // Fetch character
+        Console.Write("Please enter the max amount: "); // Prompt the User to enter the max amount 
+        int maxValue = Convert.ToInt32(Console.ReadLine()); // Fetch amount
 
-        for (int i = 0; i <= maxValue; i++)
+        for (int i = 0; i <= maxValue; i++) // For loop to generate each line
         {
            Console.WriteLine(clone(character, i));
         }
     }
     
 
-    static string clone(char c, int value)
+    static string clone(char c, int value) // Method/Function for generating lines
     {
         string cloned ="";
         for(int i = 0; i < value; i++)
